9d55d080e8416c3b082d3013cdc7dcdb3e3615ad
C-H716 <1536152356@qq.com>
2026-07-21 23:17:00 +0800

    2.0.2 fix: 上传按钮位置错误

--- src/main/java/com/gali/ae2_auto_pattern_upload/client/event/GuiUploadButtonHandler.java
+++ src/main/java/com/gali/ae2_auto_pattern_upload/client/event/GuiUploadButtonHandler.java
@@ -2,19 +2,18 @@ package com.gali.ae2_auto_pattern_upload.client.event;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import org.lwjgl.input.Keyboard;
 
 import com.gali.ae2_auto_pattern_upload.client.gui.UploadPatternButton;
+import com.gali.ae2_auto_pattern_upload.mixin.ae2.accessor.GuiContainerAccessor;
 import com.gali.ae2_auto_pattern_upload.network.ModNetwork;
 import com.gali.ae2_auto_pattern_upload.network.provider.RequestProvidersListPacket;
 import com.gali.ae2_auto_pattern_upload.network.upload.RecallLastUploadedPatternPacket;
 
 import appeng.client.gui.implementations.GuiPatternTerm;
-import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class GuiUploadButtonHandler {
@@ -33,27 +32,15 @@ public class GuiUploadButtonHandler {
             return;
         }
 
-        int guiLeft = getGuiLeft(container);
-        int guiTop = getGuiTop(container);
-        int ySize = getYSize(container);
-
-        int encodeButtonX = guiLeft + 148;
+        GuiContainerAccessor accessor = (GuiContainerAccessor) container;
+        int guiLeft = accessor.getGuiLeft();
+        int guiTop = accessor.getGuiTop();
+        int ySize = accessor.getYSize();
+        int encodeButtonX = guiLeft + 147;
         int encodeButtonY = guiTop + ySize - 142;
 
         this.uploadButton = new UploadPatternButton(BUTTON_UPLOAD_ID, encodeButtonX - 13, encodeButtonY + 2);
         event.buttonList.add(this.uploadButton);
-    }
-
-    private int getGuiLeft(GuiContainer gui) {
-        return ObfuscationReflectionHelper.getPrivateValue(GuiContainer.class, gui, "guiLeft", "field_147003_i");
-    }
-
-    private int getGuiTop(GuiContainer gui) {
-        return ObfuscationReflectionHelper.getPrivateValue(GuiContainer.class, gui, "guiTop", "field_147009_r");
-    }
-
-    private int getYSize(GuiContainer gui) {
-        return ObfuscationReflectionHelper.getPrivateValue(GuiContainer.class, gui, "ySize", "field_146999_f");
     }
 
     @SubscribeEvent
